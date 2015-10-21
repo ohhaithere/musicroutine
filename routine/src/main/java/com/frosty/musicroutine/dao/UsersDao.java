9fe1031639def09892cf9e6b89a13e2b16aed296
frostmaster <frostmasterwork@gmail.com>
2015-10-21 09:43:10 +0300

    User dao first version

---
+++ routine/src/main/java/com/frosty/musicroutine/dao/UsersDao.java
@@ -0,0 +1,17 @@
+package com.frosty.musicroutine.dao;
+
+import java.util.List;
+
+import com.frosty.musicroutine.domain.User;
+
+public interface UsersDao {
+	public void addUser(User user);
+	
+	public List<User> listUsers();
+	
+	public void removeUser(Integer id);
+	
+	public void updateUser(User user);
+	
+	public User getUser(Integer id);
+}
---
+++ routine/src/main/java/com/frosty/musicroutine/dao/impl/UserDaoImpl.java
@@ -0,0 +1,40 @@
+package com.frosty.musicroutine.dao.impl;
+
+import java.util.List;
+
+import com.frosty.musicroutine.dao.UsersDao;
+import com.frosty.musicroutine.domain.User;
+
+public class UserDaoImpl implements UsersDao {
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> listUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeUser(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
