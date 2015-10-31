c56c514999ed0e9537eee8e1346dee3f9652de16
lal@lal.ru <lal@lal.ru>
2015-11-01 02:44:44 +0300

    user sessions

---
+++ routine/src/main/java/com/frosty/musicroutine/domain/UserSession.java
@@ -0,0 +1,62 @@
+package com.frosty.musicroutine.domain;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sessions")
+public class UserSession {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name = "token")
+	private String token;
+	
+	@Column(name = "loginDate")
+	private Date loginDate;
+	
+	@Column(name = "user_id")
+	private Integer user_id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Date getLoginDate() {
+		return loginDate;
+	}
+
+	public void setLoginDate(Date loginDate) {
+		this.loginDate = loginDate;
+	}
+
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+	
+
+}
