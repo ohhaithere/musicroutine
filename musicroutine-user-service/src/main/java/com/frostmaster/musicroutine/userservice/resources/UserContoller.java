f93dd78a47c2a45df29c5b54caffaa870b20dc33
MOSCOW\Igor_Usachev <Igor_Usachev@epam.com>
2017-04-19 15:51:16 +0300

    Add remaining CRUD operations

--- musicroutine-user-service/src/main/java/com/frostmaster/musicroutine/userservice/resources/UserContoller.java
+++ musicroutine-user-service/src/main/java/com/frostmaster/musicroutine/userservice/resources/UserContoller.java
@@ -6,10 +6,7 @@ import com.frostmaster.musicroutine.userservice.domain.service.UserDispatcher;
 import com.frostmaster.musicroutine.userservice.domain.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Igor_Usachev on 3/6/2017.
@@ -35,9 +32,30 @@ public class UserContoller {
         userDispatcher.dispatch(user);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public User get() throws Exception {
-        return userService.findByEmail("email@gay.ru");
+    @RequestMapping("/{id}")
+    public User get(@PathVariable("id") String id) throws Exception {
+        User user = userService.findById(id);
+        return user;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public void update(@RequestBody UserVO userVO) throws Exception {
+        User user = new User();
+
+        BeanUtils.copyProperties(userVO, user);
+
+        userService.update(user);
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void delete(@RequestBody UserVO userVO) throws Exception {
+        User user = new User();
+
+        BeanUtils.copyProperties(userVO, user);
+
+        userService.delete(user);
+
     }
 
 }
