68fc69254ebb6e5c38a95655296ea2418131d5fe
MOSCOW\Igor_Usachev <Igor_Usachev@epam.com>
2017-08-11 11:32:40 +0300

    Fix hystrix

--- monitoring/src/main/java/com/frostmaster/musicroutine/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/com/frostmaster/musicroutine/monitoring/MonitoringApplication.java
@@ -2,15 +2,11 @@ package com.frostmaster.musicroutine.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableConfigurationProperties
-@EnableTurbine
-@EnableHystrix
+@EnableTurbineStream
 @EnableHystrixDashboard
 public class MonitoringApplication {
     public static void main(String[] args) {
--- musicroutine-user-service/src/main/java/com/frostmaster/musicroutine/userservice/App.java
+++ musicroutine-user-service/src/main/java/com/frostmaster/musicroutine/userservice/App.java
@@ -26,6 +26,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class App {
 
     public static void main(String[] args) {
+        Boolean bool = true && false && true;
         SpringApplication.run(App.class, args);
     }
 
